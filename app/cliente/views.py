893e204db2ea4fc15d09e4bf6525d57ed32d6a6a
steven23s <steven23s@outlook.es>
2020-12-07 22:14:43 -0500

    version 2

--- app/cliente/views.py
+++ app/cliente/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+
+from app.cliente.form import clienteForm
+from app.cliente.models import cliente
+from django.views.generic import *
+
 
 # Create your views here.
+class cliente_list(ListView):
+    model = cliente
+    template_name = 'cliente/cliente_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Listado de Clientes'
+        context['nuevo'] = reverse_lazy('cliente:crear')
+        context['url'] = reverse_lazy('cliente:lista')
+        context['entidad'] = 'Cliente'
+        return context
+
+
+class cliente_create(CreateView):
+    model = cliente
+    form_class = clienteForm
+    template_name = 'cliente/cliente_form.html'
+    success_url = reverse_lazy('cliente:lista')
+
+
+def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context['title'] = 'Registro de Cliente'
+    context['url'] = reverse_lazy('cliente:lista')
+    context['entidad'] = 'Cliente'
+    return context
+
+
+class cliente_update(UpdateView):
+    model = cliente
+    form_class = clienteForm
+    template_name = 'cliente/cliente_form.html'
+    success_url = reverse_lazy('cliente:lista')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Edicion de Cliente'
+        context['url'] = reverse_lazy('cliente:lista')
+        context['entidad'] = 'Cliente'
+        return context
+
+
+class cliente_delete(DeleteView):
+    model = cliente
+    form_class = clienteForm
+    template_name = 'form_delete.html'
+    success_url = reverse_lazy('cliente:lista')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Eliminacion de Cliente'
+        context['entidad'] = 'Cliente'
+
+        return context
--- sistema_yamaha/urls.py
+++ sistema_yamaha/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+from app.cliente.url import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cliente/', include('app.cliente.url', namespace='cliente'))
+
 ]
